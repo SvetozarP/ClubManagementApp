7050c2050bbd9d6d536ace83b769529550a04b01
SvetozarP <145108913+SvetozarP@users.noreply.github.com>
2024-11-17 19:25:23 +0000

    Secret key rotated

--- ArcheryApp/settings.py
+++ ArcheryApp/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 import os
 from pathlib import Path
 from decouple import config
+from django.core.management.utils import get_random_secret_key
 
 from django.urls import reverse_lazy
 
@@ -23,7 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = config('DJANGO_SECRET_KEY', default='default-unsafe-secret-key')
+NEW_SECRET_KEY = config('DJANGO_NEW_SECRET_KEY', default=get_random_secret_key())
+
+SECRET_KEY = NEW_SECRET_KEY
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
